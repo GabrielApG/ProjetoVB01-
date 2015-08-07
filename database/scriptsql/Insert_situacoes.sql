835e2a0be32c55b14727110bfe6accf749f965c1
Gabriel Ap <gabrielstigma@yahoo.com.br>
2015-08-06 21:59:08 -0300

    Antes de integração Thiago

--- database/scriptsql/Insert_situacoes.sql
+++ database/scriptsql/Insert_situacoes.sql
@@ -4,7 +4,3 @@ INSERT INTO `situacoes` (`id`, `nome`, `created_at`, `updated_at`) VALUES
 (2, 'Compra Confirmada', '2015-08-04 18:52:53', '2015-08-04 18:52:53'),
 (3, 'Em Viagem', '2015-08-04 18:52:53', '2015-08-04 18:52:53');
 
-INSERT INTO `situacoes` (`id`, `nome`, `created_at`, `updated_at`) VALUES
-(1, 'Aguardando Orçamento', '2015-08-04 18:52:53', '2015-08-04 18:52:53'),
-(2, 'Compra Confirmada', '2015-08-04 18:52:53', '2015-08-04 18:52:53'),
-(3, 'Em Viagem', '2015-08-04 18:52:53', '2015-08-04 18:52:53');
